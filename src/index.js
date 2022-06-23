07dc11ce9ff756a5e9f56cb635900d4e3da33312
Carter Stonesifer <carter.stonesifer@duke.edu>
2022-06-23 17:25:05 -0400

    Added MetaMask Extension Button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 //Entry point to the application: this runs first
-
+import MetaMaskOnboarding from '@metamask.onboarding';
+import { Button } from 'bootstrap';
+const onboarding = new MetaMaskOnboarding();
 import Web3 from 'web3';
 // import 'bootstrap/dist/css/bootstrap.css';
+
+
+const isMetaMaskInstalled = () => {
+    const {ethereum} = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+}
+const onClickInstallMetaMask = () => {
+    onboarding.startOnboarding();
+    //loader.style.display = 'block';
+}
+
+async function connectWallet(){
+    return await ethereum.request({method: 'eth_accounts'});
+}
+
+
+const MetaMaskClientCheck = () => {
+    if(!isMetaMaskInstalled()){
+        statusText.innerText = 'You need to Install a Wallet';
+
+    }else{
+        connectWallet().then((accounts) => {
+            if(accounts && accounts[0]>0){
+                connected(accounts)
+            } else {
+                statusText.innerHTML = 'Connect you Wallet'
+            }
+        })
+    }
+}
+MetaMaskClientCheck();
+
+Button.addEventListener('click', async () =>{
+    Button.style.backgroundColor = '#cccccc';
+    //loader.style.display = 'block';
+    try{
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'})
+        connected(accounts)
+    } catch (error){
+        console.error(error);
+    }
+})
+
+function doColor1(){
+  var cc1 = document.getElementById("div1");
+  cc1.style.backgroundColor ="blue";
+}
 
 //import the json file created by the solidity file
 
